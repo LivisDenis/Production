67d83c35bf4f9c645fe9d7f03566b8eeb40f0873
Denis Chumachenko <deniskachumachenko1@gmail.com>
2023-08-14 04:51:11 +0300

    Customizing the webpack and decomposing it

---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFn(args: string): string {
+    console.log('TEST LOG')
+
+    return args
+}
---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import webpack from "webpack";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildPath} from "./config/build/types/config";
+
+export default (env: BuildEnv): webpack.Configuration => {
+
+    const paths: BuildPath = {
+        entry: path.resolve(__dirname, "src", "index.ts"),
+        build: path.resolve(__dirname, "build"),
+        html: path.resolve(__dirname, "public", "index.html")
+    }
+
+    const mode = env.mode ?? "development"
+    const PORT = env.port ?? 3001
+
+    const isDev = mode === "development"
+
+    return buildWebpackConfig({
+        mode: mode,
+        paths: paths,
+        isDev,
+        port: PORT
+    });
+};
