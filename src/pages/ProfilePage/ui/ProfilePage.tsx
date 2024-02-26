535e6358993b3d77a29ce456fe50e0de9df384b3
Denis Chumachenko <deniskachumachenko1@gmail.com>
2024-02-26 20:16:18 +0200

    fixed errors

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -2,10 +2,17 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { Page } from 'widgets/Page/Page';
+import { Text } from 'shared/ui/Text/Text';
 import { EditableProfileCard } from 'features/editableProfileCard';
+import { useTranslation } from 'react-i18next';
 
 const ProfilePage = () => {
   const { id } = useParams<{id: string}>();
+  const { t } = useTranslation('profile');
+
+  if (!id) {
+    return <Text title={t('Профиль не найден')} />;
+  }
 
   return (
     <Page className={classNames('', {})}>
